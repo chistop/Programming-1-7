375f491cf601a989e839ae55b19e34f40539586a
chistop <112727275+chistop@users.noreply.github.com>
2022-09-27 19:34:49 +0000

    update

--- kareltemplate2.py
+++ kareltemplate2.py
@@ -1,40 +1,38 @@
-pipfrom stanfordkarel import *
+from stanfordkarel import *
 
 
 class ktools:
-  def m(self):
-  """Shorthand for Move"""
-  move()
 
-  def tl(self):
-  """Trun Left"""
-  trun_left()
+    def m(self):
+        """Shorthand for Move"""
+        move()
 
-  def tr(self):
-  """Trun Right"""
-  self.tl()
-  self.tl()
-  self.tl()
+    def tl(self):
+        """Turn Left"""
+        turn_left()
 
-  def ta(self):
-  """Turn Around"""
-  slef.tl()
-  slef.tl()
+    def tr(self):
+        """Turn Right"""
+        self.tl()
+        self.tl()
+        self.tl()
 
-  def put2(self):
-    """Put 2 beepers in line"""
-    self.put()
-    self.m()
-    self.put()
+    def put(self):
+        """Put beeper"""
+        put_beeper()
 
+    def ta(self):
+        """Turn Around"""
+        self.tl()
+        self.tl()
 
+    def put2(self):
+        """Put 2 beepers in line"""
+        self.put()
+        self.m()
+        self.put()
 
-
-
-
-
-
-  def put5(self):
+def put5(self):
     """Put 5 beepers in line"""
     self.put2()
     self.m()
@@ -43,37 +41,37 @@ class ktools:
     self.put()
 
 def h(self):
-  """Print H using beepers"""
-  self.tl()
-  self.put5()
-  self.tr()
-  self.m()
-  self.m()
-  self.m()
-  self.tr()
-  self.put5()
-  self.ta()
-  self.m()
-  self.m()
-  self.tl()
-  self.m()
-  self.put2()
-  self.tl()
-  self.m()
-  self.m()
-  self.tl()
-  self.m()
-  self.m()
-  self.m()
-  self.m()
+    """Print H using beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
 
-  
-  def main():
+
+def main():
     """ Karel code goes here! """
     kt = ktools()
-    kth()
+    kt.h()
     pass
 
 
-  if __name__ == "__main__":
-  run_karel_program()
+if __name__ == "__main__":
+    run_karel_program()
