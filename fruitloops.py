41fb4f4d0c9c51f585c420f7ba2e67634a352b84
chistop <112727275+chistop@users.noreply.github.com>
2022-11-14 20:22:59 +0000

    update

---
+++ fruitloops.py
@@ -0,0 +1,13 @@
+colors =["red" , "green" , "blue"]
+print(colors)
+print(colors[1])
+
+fruits = ["apple" , "banana" , "orange" , "kiwi" , "mango"]
+for item in fruits:
+  print(item)
+
+for index in range(len(fruits)):
+  print(fruits[index])
+
+lastfruits = fruits[len(fruits) - 1]
+print(lastfruits)
---
+++ thing.py
@@ -0,0 +1,5 @@
+cris = input("Hi! what's your name?")
+print("Nice to meet you" + cris + "!")
+
+age = input("How old are you")
+print("So, you are already " + str(14) + " years old, " + cris + "!")
